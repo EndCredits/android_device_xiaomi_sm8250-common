b8071b9c761432df42337ad14e110a796a233675
EndCredits <endcredits@crepuscular-aosp.icu>
2023-08-22 13:35:05 +0800

    sm8250-common: Drop duplicated dex2oat compile profile
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- common.mk
+++ common.mk
@@ -483,11 +483,6 @@ PRODUCT_SOONG_NAMESPACES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-
-# Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
-
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
