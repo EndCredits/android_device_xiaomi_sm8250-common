a7fdf675a1c4c98e5c568056f4882374d59813b3
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-11-12 21:07:31 +0100

    sm8250-common: Keep building health HIDL interface lib in vendor
    * GNSS HAL needs it
    
    Change-Id: Id2e1b2844d195d41fc67a0224e0dc39573f7702e

--- common.mk
+++ common.mk
@@ -285,6 +285,9 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.example_recovery
 endif
 
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1.vendor
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
