2be995d3e71d5927ef265580d8e77e67be35c4ad
EndCredits <endcredits@crepuscular-aosp.icu>
2023-11-12 23:22:43 +0800

    sm8250-common: Provide libprotobuf-cpp-full and its vendorcompat varient
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- common.mk
+++ common.mk
@@ -404,6 +404,8 @@ PRODUCT_PACKAGES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 ifneq ($(TARGET_BOARD_PLATFORM), lito)
