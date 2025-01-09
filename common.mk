06ef10c0c9f9baf40c35671c1e63218affd17a9a
LuK1337 <priv.luk@gmail.com>
2025-01-09 12:57:53 +0100

    sm8250-common: Build gralloc.qcom
    Camera HAL wants it and crashes if it loads default gralloc
    implementation instead.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/8059
    Change-Id: I47965713c822cfdfc369975f8975f21f70133608
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- common.mk
+++ common.mk
@@ -207,6 +207,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
+PRODUCT_PACKAGES += \
+    gralloc.qcom
+
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.supports_background_blur=1 \
     ro.sf.blurs_are_expensive=1
