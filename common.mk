0e2a3fa1fc9088d11c279d664bb3a4159882654b
hoang1007 <hoangvuhuy1007@gmail.com>
2025-01-14 13:20:10 +0700

    sm8250-common: Move to Xiaomi fingerprint AIDL
    Change-Id: I68dba7184034fd37454c7d32a065f041cb611625
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- common.mk
+++ common.mk
@@ -224,7 +224,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint-service.xiaomi
 
 ifeq ($(TARGET_HAS_UDFPS),true)
 PRODUCT_PACKAGES += \
